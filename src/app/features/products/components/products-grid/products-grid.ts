351b6eefafb76a0691ad26304c1a93f53ffc6b79
GiselleBarbosa <g88barbosa@gmail.com>
2026-04-20 20:42:30 -0300

    aula 13: adicionando signal para busca de produtos

--- src/app/features/products/components/products-grid/products-grid.ts
+++ src/app/features/products/components/products-grid/products-grid.ts
@@ -3,17 +3,30 @@ import { MatIcon } from '@angular/material/icon';
 import { Produto } from '../../interfaces/produto';
 import { produtosMock } from '../../mock/produtos-mock';
 import { ProductCard } from '../product-card/product-card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard, MatIcon],
+  imports: [ProductCard, MatIcon, MatInputModule, MatFormFieldModule, FormsModule],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
 export class ProductsGrid {
   protected readonly produtos = signal<Produto[]>(produtosMock);
+  protected readonly termoPesquisa = signal('');
 
   addToCart(productName: string) {
+    console.log(this.termoPesquisa());
     alert(`${productName} foi adicionado ao carrinho!`);
+  }
+
+  clearSearch() {
+    this.termoPesquisa.set('');
+  }
+
+  trimSearch() {
+    this.termoPesquisa.update((value) => value.trim());
   }
 }
